72fbd0ba9c0f4365b267c726e7347bcffe40f600
hebaihe55 <hebaihe55@qq.com>
2015-09-01 12:28:43 +0800

    Initial commit

--- QLWeiXin/QLWeiXin/lsjyWeb/login.aspx.cs
+++ QLWeiXin/QLWeiXin/lsjyWeb/login.aspx.cs
@@ -12,7 +12,7 @@ namespace QLWeiXin.lsjyWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            this.username.Text = "aa";
+            this.username.Text = "bbaaaa";
 
 
 
